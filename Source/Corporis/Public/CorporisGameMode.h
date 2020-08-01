0822c0b16dbb216dd1903804eb361cadd46ad240
Choyeon <Choyeon-engr@gmail.com>
2020-08-01 20:13:18 +0900

    Create: CorporisGameMode.h

---
+++ Source/Corporis/Public/CorporisGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Corporis.h"
+#include "GameFramework/GameModeBase.h"
+#include "CorporisGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORPORIS_API ACorporisGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+public:
+    ACorporisGameMode();
+};
