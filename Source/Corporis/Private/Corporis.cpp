2a9c96878e90889679296d88ebc77a959d5eb21f
Choyeon <Choyeon-engr@gmail.com>
2020-08-01 14:27:15 +0900

    Create: Corporis.cpp

---
+++ Source/Corporis/Private/Corporis.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Corporis.h"
+#include "Modules/ModuleManager.h"
+
+DEFINE_LOG_CATEGORY(Corporis);
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Corporis, "Corporis" );
