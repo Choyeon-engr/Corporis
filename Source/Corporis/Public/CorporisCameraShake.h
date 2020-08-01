1c071d55639714657f608c88821bc3c46d9ff9d7
Choyeon <Choyeon-engr@gmail.com>
2020-08-01 14:29:15 +0900

    Create: CorporisCameraShake.h

---
+++ Source/Corporis/Public/CorporisCameraShake.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Corporis.h"
+#include "Camera/CameraShake.h"
+#include "CorporisCameraShake.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORPORIS_API UCorporisCameraShake : public UCameraShake
+{
+	GENERATED_BODY()
+	
+public:
+    UCorporisCameraShake();
+};
