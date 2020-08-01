da22f3216f9f4d34da29c83e19a7ec2855596d52
Choyeon <Choyeon-engr@gmail.com>
2020-08-01 14:29:43 +0900

    Create: BTService_Detect.h

---
+++ Source/Corporis/Public/BTService_Detect.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Corporis.h"
+#include "BehaviorTree/BTService.h"
+#include "BTService_Detect.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class CORPORIS_API UBTService_Detect : public UBTService
+{
+    GENERATED_BODY()
+    
+public:
+    UBTService_Detect();
+    
+protected:
+    virtual void TickNode(UBehaviorTreeComponent & OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+};
