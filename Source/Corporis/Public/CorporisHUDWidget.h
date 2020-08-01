89eb015c7dbeccdf9e03ac9317b6ecdc2c02579d
Choyeon <Choyeon-engr@gmail.com>
2020-08-01 20:51:39 +0900

    Create: CorporisHUDWidget.h

---
+++ Source/Corporis/Public/CorporisHUDWidget.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Corporis.h"
+#include "Blueprint/UserWidget.h"
+#include "CorporisHUDWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CORPORIS_API UCorporisHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+    void BindChampionStat(class ACorporisChampion* ChampionStat);
+    
+protected:
+    virtual void NativeConstruct() override;
+    void UpdateChampionStat();
+    
+private:
+    class ACorporisChampion* CurrentChampionStat;
+    
+    UPROPERTY()
+    class UProgressBar* HealthBar;
+};
