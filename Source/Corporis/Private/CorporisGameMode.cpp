8544f11cb3b162fce3ad52d1b37f2eb86779f45b
Choyeon <Choyeon-engr@gmail.com>
2020-08-01 20:13:27 +0900

    Create: CorporisGameMode.cpp

---
+++ Source/Corporis/Private/CorporisGameMode.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CorporisGameMode.h"
+#include "CorporisChampion.h"
+#include "CorporisPlayerController.h"
+
+ACorporisGameMode::ACorporisGameMode()
+{
+    static ConstructorHelpers::FClassFinder<ACorporisChampion> PLAYER_CHAMPION_C(TEXT("/Game/ParagonLtBelica/Characters/Heroes/Belica/LtBelicaPlayerCharacter.LtBelicaPlayerCharacter_C"));
+    if (PLAYER_CHAMPION_C.Succeeded())
+        DefaultPawnClass = PLAYER_CHAMPION_C.Class;
+    
+    PlayerControllerClass = ACorporisPlayerController::StaticClass();
+}
