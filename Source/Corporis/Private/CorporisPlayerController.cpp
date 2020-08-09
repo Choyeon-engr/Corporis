609b2930186520f38df8866879b13dcd486dfcc0
Choyeon <Choyeon.engr@gmail.com>
2020-08-09 17:30:26 +0900

    Feat: Pause

--- Source/Corporis/Private/CorporisPlayerController.cpp
+++ Source/Corporis/Private/CorporisPlayerController.cpp
@@ -1,19 +1,53 @@
 #include "CorporisPlayerController.h"
+#include "CorporisPauseWidget.h"
 
 ACorporisPlayerController::ACorporisPlayerController()
 {
     static ConstructorHelpers::FClassFinder<UCorporisHUDWidget> UI_HUD_C(TEXT("/Game/Blueprints/UI/BP_UI_HUD.BP_UI_HUD_C"));
     if (UI_HUD_C.Succeeded())
         HUDWidgetClass = UI_HUD_C.Class;
+    
+    static ConstructorHelpers::FClassFinder<UCorporisPauseWidget> UI_PAUSE_C(TEXT("/Game/Blueprints/UI/BP_UI_Pause.BP_UI_Pause_C"));
+    if (UI_PAUSE_C.Succeeded())
+        PauseWidgetClass = UI_PAUSE_C.Class;
 }
 
 void ACorporisPlayerController::BeginPlay()
 {
     Super::BeginPlay();
     
-    FInputModeGameOnly GameMode;
-    SetInputMode(GameMode);
+    ChangeInputMode(true);
     
     HUDWidget = CreateWidget<UCorporisHUDWidget>(this, HUDWidgetClass);
     HUDWidget->AddToViewport();
+}
+
+void ACorporisPlayerController::SetupInputComponent()
+{
+    Super::SetupInputComponent();
+    
+    InputComponent->BindAction(TEXT("Pause"), EInputEvent::IE_Pressed, this, &ACorporisPlayerController::Pause);
+}
+
+void ACorporisPlayerController::ChangeInputMode(bool bIsGameMode)
+{
+    if (bIsGameMode)
+    {
+        SetInputMode(GameMode);
+        bShowMouseCursor = false;
+    }
+    else
+    {
+        SetInputMode(UIMode);
+        bShowMouseCursor = true;
+    }
+}
+
+void ACorporisPlayerController::Pause()
+{
+    PauseWidget = CreateWidget<UCorporisPauseWidget>(this, PauseWidgetClass);
+    PauseWidget->AddToViewport(1);
+    
+    SetPause(true);
+    ChangeInputMode(false);
 }
--- Source/Corporis/Public/CorporisPlayerController.h
+++ Source/Corporis/Public/CorporisPlayerController.h
@@ -15,14 +15,29 @@ public:
     
     UCorporisHUDWidget* GetHUDWidget() const { return HUDWidget; }
     
+    void ChangeInputMode(bool bIsGameMode = true);
+    
 protected:
     virtual void BeginPlay() override;
+    virtual void SetupInputComponent() override;
+    
+private:
+    void Pause();
     
 protected:
     UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
     TSubclassOf<class UCorporisHUDWidget> HUDWidgetClass;
     
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = UI)
+    TSubclassOf<class UCorporisPauseWidget> PauseWidgetClass;
+    
 private:
     UPROPERTY()
     UCorporisHUDWidget* HUDWidget;
+    
+    UPROPERTY()
+    class UCorporisPauseWidget* PauseWidget;
+    
+    FInputModeGameOnly GameMode;
+    FInputModeUIOnly UIMode;
 };
